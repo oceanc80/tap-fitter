a858bf585a344ee2a4d267d677166f0776bab28c
Ish Shah <ishah@redhat.com>
2023-10-11 09:09:16 -0700

    Import generation pkg

--- cmd/tap-fitter/main.go
+++ cmd/tap-fitter/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	
+	"pkg/generation"
 
 	"github.com/operator-framework/operator-registry/alpha/template/composite"
 	"github.com/operator-framework/operator-registry/pkg/image/containerdregistry"
@@ -129,43 +131,3 @@ func (p *TapFitterTemplate) ingestComposite(cmd *cobra.Command) error {
 
 	return nil
 }
-
-func (p *TapFitterTemplate) generateDockerfile() error {
-
-	// TODO: extract enough cmd.generate.dockerfile to trigger the alpha.action.generate_dockerfile based on template fields
-	return nil
-}
-
-func (p *TapFitterTemplate) generateDevfile() error {
-	if err := p.validateFlags(); err != nil {
-		return err
-	}
-
-	t, err := template.New("devfile").Parse(devfileTmpl)
-	if err != nil {
-		// The template is hardcoded in the binary, so if
-		// there is a parse error, it was a programmer error.
-		panic(err)
-	}
-	return t.Execute(p.writer, p)
-}
-
-const devfileTmpl = `schemaVersion: 2.2.0
-metadata:
-  name: {{.Name}}
-  displayName: {{.Name}}
-  description: 'File based catalog'
-  language: fbc
-  provider: {{.Provider}}
-components:
-  - name: image-build
-    image:
-      imageName: fbc:latest
-      dockerfile:
-        uri: {{.IndexDir}}.Dockerfile
-        buildContext: {{.BuildCTX}}
-commands:
-  - id: build-image
-    apply:
-      component: image-build
-`
